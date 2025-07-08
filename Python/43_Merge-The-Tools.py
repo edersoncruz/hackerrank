b04ede827dffaeca67ee5fc612c5fca1d0b81e7b
Ederson Cruz <130115232+edersoncruz@users.noreply.github.com>
2025-07-08 13:42:36 -0300

    Create 43_Merge-The-Tools.py

---
+++ Python/43_Merge-The-Tools.py
@@ -0,0 +1,18 @@
+
+
+def merge_the_tools(string, k):
+    def remove_duplicate(part):
+        result = ''
+        for c in part:
+            if c not in result:
+                result += c
+        return result
+        
+    for i in range(0, len(string), k):
+        part = string[i:i+k]
+        print(remove_duplicate(part))
+        
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
